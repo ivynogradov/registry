3ed075e31d9fc75bed78ea53d92f9e74c5467a40
Yevhen Vynohradov <ivynogradov@n-ix.com>
2018-02-11 18:14:58 +0200

    added script file

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// define dependencies
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+// define helpers
+
+// define common variables
+
+// Read the registry from file
+
+// Parse data
+
+// generate checks pages
+
+// generate aggregator page
